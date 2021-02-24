3a203f93a900429a7faedb3fe4f107b595fe1263
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-02-24 20:32:35 +0300

    Kendi Dictionary'ni Yapma

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] _Karray;
+        V[] _Varray;
+
+        K[] _KtempArray;
+        V[] _VtempArray;
+        public MyDictionary()
+        {
+            _Karray = new K[0];
+            _Varray = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            _KtempArray = _Karray;
+            _VtempArray = _Varray;
+
+            _Karray = new K[_Karray.Length + 1];
+            _Varray = new V[_Varray.Length + 1];
+
+            for (int i = 0; i < _KtempArray.Length; i++)
+            {
+                _Karray[i] = _KtempArray[i];
+            }
+            for (int i = 0; i < _VtempArray.Length; i++)
+            {
+                _Varray[i] = _VtempArray[i];
+            }
+
+            _Karray[_Karray.Length - 1] = key;
+            _Varray[_Varray.Length - 1] = value;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using MyDictionary;
+using System;
+using System.Collections.Generic;
+
+namespace IDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> adetUrun = new MyDictionary<int, string>();
+            adetUrun.Add(2, "Pantolon");
+            adetUrun.Add(3, "Elbise");
+            adetUrun.Add(5, "Etek");
+            adetUrun.Add(7, "Hırka");
+
+        }
+    }
+
+}
